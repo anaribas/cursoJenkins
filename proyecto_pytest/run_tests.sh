dd8992c0a1be192d47b4d761a555acba260aabf5
Anna Ribas <annaribas@PC68833.local>
2026-07-15 12:09:27 +0200

    Corrigiendo errores de sintaxi shell

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -3,15 +3,15 @@
 echo "Iniciando ejecución de pruebas en Jenkins..."
 
 # Verificar si el entorno virtual existe
-if [! -d "venv"]; then
+if [ ! -d "venv" ]; then
   echo "Entorno virtual no encontrado. Creándolo..."
   python3 -m venv venv
 fi
 
 # Activar el entorno virtual correctamente
-if [ -f "venv/bin/activate"]; then
+if [ -f "venv/bin/activate" ]; then
   source venv/bin/activate
-elif [ -f "venv/Scripts/activate"]; then # Para entornos Windows
+elif [ -f "venv/Scripts/activate" ]; then # Para entornos Windows
   source venv/Scripts/activate
 else
   echo "Error: No se pudo activar el entorno virtual."
